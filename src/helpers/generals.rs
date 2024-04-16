dfba7c36e53b037ff6cb7b3d297466f461c23e03
kharle14 <enriquezkharl14@gmail.com>
2024-04-16 22:42:05 +0800

    AI function

--- src/helpers/generals.rs
+++ src/helpers/generals.rs
@@ -1,4 +1,6 @@
-use crate::models::general::llm::Message;
+use crate::{apis::call_request::call_gpt, models::general::llm::Message};
+
+use super::command_line::PrintCommand;
 
 // Extend ai function to encourage specific output
 pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -> Message {
@@ -20,6 +22,31 @@ pub fn extend_ai_function(ai_func: fn(&str) -> &'static str, func_input: &str) -
     }
 }
 
+// Performs call to GPT
+pub async fn ai_task_request(
+    msg_context: String,
+    agent_position: &str,
+    agent_operation: &str,
+    callback_fn: for<'a> fn(&'a str) -> &'static str,
+) -> String {
+    // Extend AI function
+    let extended_msg: Message = extend_ai_function(callback_fn, &msg_context);
+
+    PrintCommand::AICall.print_agent_message(agent_position, agent_operation);
+
+    // Get GPT response
+    let gpt_response_result: Result<String, Box<dyn std::error::Error + Send>> =
+        call_gpt(vec![extended_msg.clone()]).await;
+
+    // Handle Success
+    match gpt_response_result {
+        Ok(gpt_response) => gpt_response,
+        Err(_) => call_gpt(vec![extended_msg.clone()])
+            .await
+            .expect("Failed twice to call GPT"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +57,20 @@ mod tests {
         let extended_msg = extend_ai_function(convert_user_input_to_goal, "dummy");
 
         assert_eq!(extended_msg.role, "system".to_string())
+    }
+
+    #[tokio::test]
+    async fn test_ai_task_request() {
+        let ai_func_param = "build be a web server for making stock price api requests".to_string();
+
+        let res = ai_task_request(
+            ai_func_param,
+            "Managing Agent",
+            "Defining user requirements",
+            convert_user_input_to_goal,
+        )
+        .await;
+
+        assert!(res.len() > 20);
     }
 }
